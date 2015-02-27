2d6befa22f4bce6eed357e7a0d4332efc7b30356
carrieb <carolyn@i-110561.local>
2015-02-27 16:15:11 -0600

    Updated crawler to be cleaner
    - Moved scripts out of literal strings and into files
    - Separated into helpful methods
    - Added main

--- crawler/ffcrawler.py
+++ crawler/ffcrawler.py
@@ -1,27 +1,72 @@
 from selenium import webdriver
 import os
 import time
+import sys
+from string import Template
 
-artooscript = "(function(){var t={},e=!0;if('object'==typeof this.artoo&&(artoo.settings.reload||(artoo.log.verbose('artoo already exists within this page. No need to inject him again.'),artoo.loadSettings(t),artoo.exec(),e=!1)),e){var o=document.getElementsByTagName('body')[0];o||(o=document.createElement('body'),document.documentElement.appendChild(o));var a=document.createElement('script');console.log('artoo.js is loading...'),a.src='//medialab.github.io/artoo/public/dist/artoo-latest.min.js',a.type='text/javascript',a.id='artoo_injected_script',a.setAttribute('settings',JSON.stringify(t)),o.appendChild(a)}}).call(this);"
-chromedriver = "/Users/carolyn/projects/scratch/crawler/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-i = 0
-LIMIT = 10;
-while i < LIMIT:
-    myscript = "(function(){var scraper = {iterator: '.z-list', data: { title: {sel:'a.stitle'}, url:{sel:'a.stitle', method:function($) {return 'http://www.fanfiction.net'+ $(this).attr('href'); }}, author: {sel:'a[href^=\"/u/\"]'}, summary:{sel:'.z-indent', method: function($) {return $(this).clone().children().remove().end().text();}}, meta:{sel:'.z-indent .xgray'}, update_ts:{sel:'.z-indent .xgray span:first-child', method: function($) {return $(this).data('xutime');}}, publish_ts:{sel: '.z-indent .xgray span:last-child', method: function($){return $(this).data('xutime');}}}}; var data = artoo.scrape(scraper); console.log(data); artoo.savePrettyJson(data, {filename:'final_fanfic_data_"+str(i)+".json'});})()" 
-    page = "https://www.fanfiction.net/book/Harry-Potter/?&srt=1&lan=1&r=10&p=" + str(i)
-    print "Fetching " + page
-    driver.get(page)
-    print "Executing artoo ..."
-    driver.execute_script(artooscript)
-    time.sleep(2)
-    print "Executing our script ..."
-    driver.execute_script(myscript)
-    print driver.page_source[:100] + " ... "
-    print "Sleeping..."
-    i+=1
-    time.sleep(2)
-    print "Woke up!"
-driver.quit()
+## TODO: Set up command line args
+## Run all night - currently 21633 pages, not really more than like 20 pages added in a day
+
+ARTOO_PATH = 'scripts/artoo.js'
+EXTRACTION_SCRIPT_FILENAME = 'scripts/extract.js'
+DEFAULT_LIMIT = 1
+DEFAULT_CHROMEDRIVER_PATH = "/Users/carolyn/projects/scratch/crawler/chromedriver"
+DEFAULT_OUTPUT_DIR = "/Users/carolyn/projects/scratch/crawler/data"
+PAGE_URL = "https://www.fanfiction.net/book/Harry-Potter/?&srt=1&lan=1&r=10&p=%(page)d"
+DEFAULT_OUTPUT_PATH = "fanfic_data_"
+
+def main(argv):
+    print argv
+    start_time = time.time()
+    driver = setup_chromedriver()
+
+    limit = DEFAULT_LIMIT
+    artoo_path = ARTOO_PATH
+    extract_path = EXTRACTION_SCRIPT_FILENAME
+    crawl(driver, PAGE_URL, DEFAULT_LIMIT, artoo_path, extract_path, DEFAULT_OUTPUT_PATH)
+
+def crawl(driver, page_url, limit, artoo_path, extract_path, output_path):
+    start_time = time.time()
+    i = 0
+    while i < limit:
+        page = page_url % {"page" : i}
+        print "Fetching " + page
+        
+        d = driver.get(page)
+        with open(artoo_path, 'r') as f:
+            print "Executing artoo ..."
+            driver.execute_script(f.read())
+        
+        print "Sleeping for 2 seconds..."
+        time.sleep(2)
+        
+        with open(extract_path, 'r') as f:
+            print "Executing our script ..."
+            driver.execute_script(f.read() % {"counter" : i, "output_path" : output_path})
+        
+        print driver.page_source[:100] + " ... "
+        print "Sleeping for 1 second..."
+        time.sleep(1)
+
+        i+=1
+        print "Woke up!"
+    
+    elapsed_time = time.time() - start_time
+    print "Total time taken:", elapsed_time
+    driver.close()
+    driver.quit()
+    return
+
+def setup_chromedriver(driver_fn=DEFAULT_CHROMEDRIVER_PATH, output_dir=DEFAULT_OUTPUT_DIR):
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory" : output_dir}
+    print chrome_options
+    chrome_options.add_experimental_option("prefs", prefs)
+    chromedriverpath = driver_fn
+    os.environ["webdriver.chrome.driver"] = chromedriverpath
+    driver = webdriver.Chrome(executable_path=chromedriverpath, chrome_options=chrome_options)
+    return driver
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
     
--- crawler/ffprocessor.py
+++ crawler/ffprocessor.py
@@ -1,28 +1,36 @@
 import json
+import sys
 
-counter = 0
-LIMIT = 10
-compiled_file = open('compiled_fanfic.json', 'w+')
-compiled_file.write('[')
-total_len = 0
-while (counter < LIMIT):
-	json_data = open('/Users/carolyn/Downloads/final_fanfic_data_'+str(counter)+'.json')
-	data = json_data.read()
-	compiled_file.write(data.replace(']', '').replace('[',''))
-	json_data.close()
-	json_data = open('/Users/carolyn/Downloads/final_fanfic_data_'+str(counter)+'.json')
-	json_obj = json.load(json_data)
-	print data
-	print len(json_obj)
-	total_len += len(json_obj)
-	json_data.close()
-	if (counter + 1 < LIMIT):
-		compiled_file.write(',')
-	counter+=1
-compiled_file.write(']')
+def main(argv):
+	print argv
 
-compiled_file.close()
+def crawl():
+	counter = 0
+	LIMIT = 0
+	compiled_file = open('compiled_fanfic.json', 'w+')
+	compiled_file.write('[')
+	total_len = 0
+	while (counter < LIMIT):
+		print "File #" + str(counter)
+		json_data = open('/Users/carolyn/Downloads/fanfic_json-final_fanfic_data_'+str(counter)+'.json')
+		data = json_data.read()
+		compiled_file.write(data.replace(']', '').replace('[',''))
+		json_data.close()
+		json_data = open('/Users/carolyn/Downloads/fanfic_json-final_fanfic_data_'+str(counter)+'.json')
+		json_obj = json.load(json_data)
+		print len(json_obj)
+		total_len += len(json_obj)
+		json_data.close()
+		if (counter + 1 < LIMIT):
+			compiled_file.write(',')
+		counter+=1
+	compiled_file.write(']')
 
-compiled_file = open('compiled_fanfic.json')
-final_data = json.load(compiled_file)
-print len(final_data), total_len
+	compiled_file.close()
+
+	compiled_file = open('compiled_fanfic.json')
+	final_data = json.load(compiled_file)
+	print len(final_data), total_len
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
